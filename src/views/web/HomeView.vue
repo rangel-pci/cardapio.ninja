94341f61dbc4183c495ac3ecfe4ad994bb34b927
Rangel Pereira <rangel.pci@gmail.com>
2023-06-18 13:29:30 -0300

    initial commit

---
+++ src/views/web/HomeView.vue
@@ -0,0 +1,64 @@
+<script setup lang="ts">
+  const APP_NAME = import.meta.env.VITE_APP_NAME
+</script>
+
+<template>
+  <div class="bg-gray-100 pt-40" id="inicio">
+    <header class="container mx-auto bg-white fixed top-0 left-0 w-full shadow-md">
+      <nav id="nav" class="mx-auto flex">
+        <a href="#inicio" class="pt-6 pb-6">
+          <img src="@/assets/img/logo/logo_1.svg" class="rounded" :alt="APP_NAME + ' logo'" :title="APP_NAME" width="50">
+        </a>
+        <ul class="flex justify-center">
+          <li class="flex text-center">
+            <a class="flex align-items-center font-medium hover:text-[#6C5CE7] px-8" href="#">
+              <span class="my-auto">Como Funciona</span>
+            </a>
+          </li>
+          <li class="flex text-center">
+            <a class="flex align-items-center font-medium hover:text-[#6C5CE7] px-8" href="#">
+              <span class="my-auto">Vantagens</span>
+            </a>
+          </li>
+          <li class="flex text-center">
+            <a class="flex align-items-center font-medium hover:text-[#6C5CE7] px-8" href="#">
+              <span class="my-auto">Preços</span>
+            </a>
+          </li>
+          <li class="flex text-center">
+            <a class="flex align-items-center font-medium hover:text-[#6C5CE7] px-8" href="#">
+              <span class="my-auto">Login</span>
+            </a>
+          </li>
+          <li class="flex text-center w-1/4 ml-8">
+            <a class="flex align-items-center font-medium  px-8 py-2
+            text-white bg-gradient-to-br from-[#6C5CE7] to-[#55EFC4] transition-all hover:shadow-lg hover:shadow-cyan-300" href="#"
+            >
+              <span class="my-auto">🔥 Quero criar meu cardápio digital agora gratuitamente!</span>
+            </a>
+          </li>
+          <!-- <li class="flex text-center">
+            <a class="flex align-items-center font-medium  px-8 ml-4
+            text-white bg-gradient-to-tr from-[#55EFC4] to-[#6C5CE7] transition-all hover:shadow-lg hover:shadow-cyan-300" href="#"
+            >
+              <span class="my-auto">Login -></span>
+            </a>
+          </li> -->
+        </ul>
+      </nav>
+    </header>
+
+    <main>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+      dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>dsad<br>
+    </main>
+  </div>
+</template>
+
+<style scoped>
+  @import '/dist/output.css';
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/* eslint-env node */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue, js, ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
